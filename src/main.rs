334f6dc6e8cdb43f4d701cc99604b05cc7d35768
Josh Matthews <josh@joshmatthews.net>
2016-11-13 14:44:43 -0500

    Draw player. Scroll map with arrow keys.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ extern crate xnb;
 use glutin_window::GlutinWindow as Window;
 use graphics::Image;
 use image::RgbaImage;
-use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings};
+use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings, Filter, ImageSize};
 use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
@@ -19,9 +19,12 @@ use std::path::Path;
 use xnb::{XNB, Asset};
 use xnb::tide::{TileSheet, Layer};
 
+const SCALE: f64 = 2.0;
+
 pub struct App {
     gl: GlGraphics,
-    rotation: f64,
+    view_x: u32,
+    view_y: u32,
 }
 
 struct Tile<'a> {
@@ -29,17 +32,60 @@ struct Tile<'a> {
     index: u32,
 }
 
-fn image_for_tile(tile: &Tile) -> Image {
+fn image_for_tile(tile: &Tile, pos: (u32, u32), view: (u32, u32)) -> Image {
     let num_h_tiles = tile.sheet.sheet_size.0;
     let tile_w = tile.sheet.tile_size.0;
     let tile_h = tile.sheet.tile_size.1;
-    let src_x = tile.index % num_h_tiles * tile_w;
-    let src_y = tile.index / num_h_tiles * tile_h;
+    image_for_tile_reference(num_h_tiles,
+                             (tile_w, tile_h),
+                             tile.index,
+                             pos,
+                             (0, 0),
+                             view)
+}
+
+fn image_for_texture(texture: &TextureTileInfo,
+                     pos: (u32, u32),
+                     view: (u32, u32)) -> Image {
+    let num_h_tiles = texture.0.get_width() / 16;
+    image_for_tile_reference(num_h_tiles,
+                             texture.2.clone(),
+                             texture.1,
+                             pos,
+                             texture.3.clone(),
+                             view)
+}
+
+fn image_for_tile_reference(num_h_tiles: u32,
+                            (tile_w, tile_h): (u32, u32),
+                            index: u32,
+                            (x, y): (u32, u32),
+                            (off_x, off_y): (i32, i32),
+                            (view_x, view_y): (u32, u32)) -> Image {
+    let src_x = index % num_h_tiles * tile_w;
+    let src_y = index / num_h_tiles * tile_h;
     Image::new()
         .src_rect([src_x as i32,
                    src_y as i32,
                    tile_w as i32,
                    tile_h as i32])
+        .rect([((x - view_x) * 16) as f64 + off_x as f64,
+               ((y - view_y) * 16) as f64 + off_y as f64,
+               tile_w as f64,
+               tile_h as f64])
+}
+
+type TextureTileInfo = (Texture, u32, (u32, u32), (i32, i32));
+
+struct Player {
+    base: TextureTileInfo,
+    bottom: TextureTileInfo,
+    arms: TextureTileInfo,
+    pants: TextureTileInfo,
+    hairstyle: TextureTileInfo,
+    hat: TextureTileInfo,
+    shirt: TextureTileInfo,
+    accessory: TextureTileInfo,
 }
 
 impl App {
@@ -47,11 +93,14 @@ impl App {
               args: &RenderArgs,
               textures: &HashMap<String, Texture>,
               tilesheets: &[TileSheet],
+              player: &Player,
               layers: &[Layer]) {
         use graphics::*;
 
-        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        const BLACK: [f32; 4] = [0.0, 0.0, 1.0, 1.0];
 
+        let view_x = self.view_x;
+        let view_y = self.view_y;
         self.gl.draw(args.viewport(), |c, gl| {
             // Clear the screen.
             clear(BLACK, gl);
@@ -68,20 +117,56 @@ impl App {
                         sheet: tilesheet,
                         index: base_tile.get_index(),
                     };
-                    let (mut x, mut y) = base_tile.get_pos();
-                    x *= tilesheet.tile_size.0;
-                    y *= tilesheet.tile_size.1;
-                    let image = image_for_tile(&tile);
-                    let transform = c.transform.trans(x as f64, y as f64);
+                    let (x, y) = base_tile.get_pos();
+                    if x < view_x || y < view_y {
+                        continue;
+                    }
+                    let image = image_for_tile(&tile, (x, y), (view_x, view_y));
+                    let transform = c.transform.zoom(SCALE);
                     image.draw(texture, &Default::default(), transform, gl);
                 }
             }
+
+            let view = (view_x, view_y);
+            let pos = (5, 5);
+
+            let transform = c.transform.zoom(SCALE);
+
+            // Body
+            let image = image_for_texture(&player.base, pos, view);
+            image.draw(&player.base.0, &Default::default(), transform, gl);
+            let image = image_for_texture(&player.bottom, pos, view);
+            image.draw(&player.bottom.0, &Default::default(), transform, gl);
+
+            // Hair
+            let image = image_for_texture(&player.hairstyle, pos, view);
+            image.draw(&player.hairstyle.0, &Default::default(), transform, gl);
+
+            // Hat
+            let image = image_for_texture(&player.hat, pos, view);
+            image.draw(&player.hat.0, &Default::default(), transform, gl);
+
+            // Arms
+            let image = image_for_texture(&player.arms, pos, view);
+            image.draw(&player.arms.0, &Default::default(), transform, gl);
+
+            // Pants
+            let image = image_for_texture(&player.pants, pos, view);
+            image.draw(&player.pants.0, &Default::default(), transform, gl);
+
+            // Shirt
+            let image = image_for_texture(&player.shirt, pos, view);
+            image.draw(&player.shirt.0, &Default::default(), transform, gl);
+
+            // Facial accessory
+            let image = image_for_texture(&player.accessory, pos, view);
+            image.draw(&player.accessory.0, &Default::default(), transform, gl);
         });
     }
 
-    fn update(&mut self, args: &UpdateArgs) {
+    fn update(&mut self, _args: &UpdateArgs) {
         // Rotate 2 radians per second.
-        self.rotation += 2.0 * args.dt;
+        //self.rotation += 2.0 * args.dt;
     }
 }
 
@@ -101,9 +186,9 @@ fn main() {
 
     let mut args = env::args();
     let _self = args.next();
-    let map = args.next().unwrap();
+    let map = args.next().unwrap_or("Town.xnb".into());
 
-    let base = Path::new("../xnb/data/uncompressed");
+    let base = Path::new("../xnb/uncompressed/Maps");
     let f = File::open(base.join(map)).unwrap();
     let xnb = XNB::from_buffer(f).unwrap();
     let map = match xnb.primary {
@@ -111,34 +196,71 @@ fn main() {
         _ => panic!("unexpected xnb contents"),
     };
 
-    let mut tilesheets = HashMap::new();
-    for ts in &map.tilesheets {
-        let f = File::open(base.join(format!("{}.xnb", ts.image_source))).unwrap();
+    fn load_texture(base: &Path, filename: &str) -> Texture {
+        let f = File::open(base.join(filename)).unwrap();
         let xnb = XNB::from_buffer(f).unwrap();
         match xnb.primary {
             Asset::Texture2d(mut texture) => {
                 let img = RgbaImage::from_raw(texture.width as u32,
                                               texture.height as u32,
                                               texture.mip_data.remove(0)).unwrap();
-                let texture = Texture::from_image(&img, &TextureSettings::new());
-                println!("storing texture for {}", ts.id);
-                tilesheets.insert(ts.id.clone(), texture);
+                let mut settings = TextureSettings::new();
+                settings.set_filter(Filter::Nearest);
+                Texture::from_image(&img, &settings)
             }
             _ => panic!("unexpected xnb contents"),
-        };
+        }
+    }
+
+    let mut tilesheets = HashMap::new();
+    for ts in &map.tilesheets {
+        let texture = load_texture(base, &format!("{}.xnb", ts.image_source));
+        println!("storing texture for {}", ts.id);
+        tilesheets.insert(ts.id.clone(), texture);
     }
     println!("loaded {} tilesheets", tilesheets.len());
+
+    let path = Path::new("../xnb/uncompressed/Characters/Farmer");
+    let base = load_texture(path, "farmer_base.xnb");
+    let bottom = load_texture(path, "farmer_base.xnb");
+    let arms = load_texture(path, "farmer_base.xnb");
+    let pants = load_texture(path, "farmer_base.xnb");
+    let hairstyle = load_texture(path, "hairstyles.xnb");
+    let hat = load_texture(path, "hats.xnb");
+    let shirt = load_texture(path, "shirts.xnb");
+    let accessory = load_texture(path, "accessories.xnb");
+    let player = Player {
+        base: (base, 0, (16, 16), (0, 0)),
+        bottom: (bottom, 24, (16, 16), (0, 16)),
+        arms: (arms, 30, (16, 16), (0, 16)),
+        hairstyle: (hairstyle, 0, (16, 16), (0, 0)),
+        hat: (hat, 2, (20, 20), (-2, -2)),
+        pants: (pants, 42, (16, 16), (0, 16)),
+        shirt: (shirt, 0, (8, 8), (4, 15)),
+        accessory: (accessory, 0, (16, 16), (0, 3)),
+    };
 
     // Create a new game and run it.
     let mut app = App {
         gl: GlGraphics::new(opengl),
-        rotation: 0.0,
+        view_x: 0,
+        view_y: 0,
     };
 
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
+        if let Some(Button::Keyboard(k)) = e.press_args() {
+            match k {
+                Key::Left if app.view_x > 0 => app.view_x -= 1,
+                Key::Right => app.view_x += 1,
+                Key::Up if app.view_y > 0 => app.view_y -= 1,
+                Key::Down => app.view_y += 1,
+                _ => {}
+            }
+        }
+
         if let Some(r) = e.render_args() {
-            app.render(&r, &tilesheets, &map.tilesheets, &map.layers);
+            app.render(&r, &tilesheets, &map.tilesheets, &player, &map.layers);
         }
 
         if let Some(u) = e.update_args() {
